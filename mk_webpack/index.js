1df8304b296cef69a60f3b68059f6952a2049d0b
QIAOFANG\jianhua.zhu <m18375326020_1@163.com>
2019-08-14 15:58:00 +0800

    fix

---
+++ mk_webpack/index.js
@@ -0,0 +1,8 @@
+// ES Moudule 模块引入方式
+import Header from './header'
+import Content from './content'
+import Siderbar from './sidebar'
+
+new Header()
+new Content()
+new Siderbar()
---
+++ mk_webpack/sidebar.js
@@ -0,0 +1,7 @@
+function Sidebar() {
+  var dom = document.getElementById('root')
+  var sidebar = document.createElement('div')
+  sidebar.innerText = 'Sidebar'
+  dom.append(sidebar)
+}
+export default Sidebar
